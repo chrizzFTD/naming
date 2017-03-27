77e6c59476ab57c7d8fc75a0b2009b441ce9c53a
Christian López Barrón <chris.gfz@gmail.com>
2017-03-27 23:12:04 +1100

    updated release

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='naming',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     description='Object-oriented names for the digital era.',
     author='Christian López Barrón',
     author_email='chris.gfz@gmail.com',
     url='https://github.com/chrizzFTD/naming',
-    download_url='https://github.com/chrizzFTD/naming/releases/tag/0.1.1',
+    download_url='https://github.com/chrizzFTD/naming/releases/tag/0.1.2',
     classifiers=['Programming Language :: Python :: 3.6'],
     extras_require={'docs': ['sphinx_autodoc_typehints']}
 )
