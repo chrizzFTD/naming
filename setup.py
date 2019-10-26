6ebab3d7014792490eae9a9a08d985110ecce424
Christian López Barrón <chris.gfz@gmail.com>
2019-10-26 12:49:26 +1100

    update CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 
-_VERSION = '0.3.0'
+_VERSION = '0.4.0'
 setup(
     name='naming',
     version=_VERSION,
@@ -13,6 +13,10 @@ setup(
     author_email='chris.gfz@gmail.com',
     url='https://github.com/chrizzFTD/naming',
     download_url=f'https://github.com/chrizzFTD/naming/releases/tag/{_VERSION}',
-    classifiers=['Programming Language :: Python :: 3.6'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
     extras_require={'docs': ['sphinx_autodoc_typehints', 'sphinx_rtd_theme']}
 )
