6ad1830f0e8a14cb178e4d23560c91e4dbd562c4
Christian López Barrón <chris.gfz@gmail.com>
2024-04-14 11:34:02 +1000

    convenient build docs script

--- docs/source/build_docs.py
+++ docs/source/build_docs.py
@@ -2,10 +2,11 @@ import shutil
 from pathlib import Path
 from sphinx.cmd import build
 
-source_path = Path(__file__).parent
-build_path = source_path.parent / "build"
-try:
-    shutil.rmtree(build_path)
-except FileNotFoundError:
-    pass
-build.build_main([str(source_path), str(build_path)])
+if __name__ == '__main__':
+    source_root = Path(__file__).parent
+    build_root = source_root.parent / "build"
+    try:
+        shutil.rmtree(build_root)
+    except FileNotFoundError:
+        pass
+    build.build_main([str(source_root), str(build_root)])
